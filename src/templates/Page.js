0da6a93156b62d65e9bb0abb54e28c751a8195c0
Olly Evans <ollydevans@gmail.com>
2022-08-12 11:21:46 +1000

    Index page test

--- src/templates/Page.js
+++ src/templates/Page.js
@@ -1,16 +1,36 @@
 import React from "react"
+import {graphql} from "gatsby";
 import Seo from "../components/molecules/Seo";
 import SlicePicker from "../components/organisms/SlicePicker";
 
 function Page({data}) {
-    const {uid} = data.prismicPage;
     const {title, body} = data.prismicPage.data
+
     return (
         <>
-            <Seo title={title.text} />
+            <Seo title={title.text}/>
+            {body.map(slice => <SlicePicker key={slice.id} slice={slice}/>)}
         </>
     )
-}
+};
+
+export const pageQuery = graphql`
+    query($id: String!) {
+        prismicPage(id: {eq: $id}) {
+            uid
+            data {
+                title {
+                    text
+                }
+                body {
+                    ... on PrismicPageDataBodyTwoBlocks { ...TwoBlocks }
+                    ... on PrismicPageDataBodyCta { ...CTA }
+                    ... on PrismicPageDataBodyTopicList { ...TopicList }
+                }
+            }
+        }
+    }
+`
 
 export default Page
 
