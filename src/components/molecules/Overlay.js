cb543abd81c868115f8661aa815d01183fc029ec
Olly Evans <ollydevans@gmail.com>
2022-08-30 15:30:03 +1000

    Trello updates 3.0

--- src/components/molecules/Overlay.js
+++ src/components/molecules/Overlay.js
@@ -1,7 +1,8 @@
-import React from "react"
+import React, {useState} from "react"
 import {Link} from "gatsby"
 import styled from "styled-components";
 import useLockBodyScroll from "../../hooks/useLockBodyScroll";
+import Cross from "../../assets/svg/cross.inline.svg";
 
 const Holder = styled.div`
   position: fixed;
@@ -24,13 +25,37 @@ const Holder = styled.div`
 
     a {
       text-decoration: none;
+    }
 
-      li {
-        margin: 0;
-        padding: 0.75rem;
-        border-radius: 5rem;
+    li {
+      margin: 0;
+      padding: 0.75rem;
+      border-radius: 5rem;
+      background-color: ${props => props.theme.colours.lightgrey};
+      text-align: center;
+      cursor: pointer;
+    }
+
+    .content {
+      display: flex;
+      background-color: ${props => props.theme.colours.lightgrey};
+      border-radius: 5rem;
+      justify-content: space-between;
+      padding: calc(0.5rem - 5px);
+
+      div {
+        display: flex;
+        column-gap: 1rem;
+      }
+
+      .button {
         background-color: ${props => props.theme.colours.lightgrey};
-        text-align: center;
+        border-radius: 5rem;
+      }
+
+      a:hover {
+        background-color: ${props => props.theme.colours.lightgrey};
+        border-radius: 5rem;
       }
     }
 
@@ -38,6 +63,8 @@ const Holder = styled.div`
 `
 
 function Overlay({hide}) {
+    const [toggle, setToggle] = useState(false)
+
     useLockBodyScroll()
     return (
         <Holder>
@@ -45,9 +72,24 @@ function Overlay({hide}) {
                 <Link onClick={hide} to="/">
                     <li>Start your free trial today</li>
                 </Link>
-                <Link to="/products">
-                    <li>Products</li>
-                </Link>
+
+                <li onClick={() => setToggle(!toggle)}>Products</li>
+                {toggle && <div className="content">
+                    <div>
+                        <Link onClick={hide} to="/air">
+                            <li>AiR</li>
+                        </Link>
+                        <Link onClick={hide} to="/stream">
+                            <li>Stream</li>
+                        </Link>
+                        <Link onClick={hide} to="/enterprise">
+                            <li>Enterprise</li>
+                        </Link>
+                    </div>
+                    <button className="button icon" onClick={() => setToggle(false)}>
+                        <Cross/>
+                    </button>
+                </div>}
                 <Link onClick={hide} to="/plans">
                     <li>Plans</li>
                 </Link>
